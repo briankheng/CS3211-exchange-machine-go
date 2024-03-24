dc3152f0139c9664cafecf1639fe916aaa08f65c
Brian Kheng <briankheng63@gmail.com>
2024-03-24 22:27:56 +0800

    feat: done

--- engine.go
+++ engine.go
@@ -14,126 +14,127 @@ import (
 type Engine struct{}
 
 type inputWithLock struct {
-	in input
-	// mtx chan struct{}
+	in  input
+	mtx chan struct{}
 }
 
-var daemonCh = make(chan input)
+var daemonCh = make(chan inputWithLock)
 
 func daemon() {
-	nameToInstrumentCh := make(map[string]chan input)
-	idToInstrumentCh := make(map[uint32]chan input)
+	nameToInstrumentCh := make(map[string]chan inputWithLock)
+	idToInstrumentCh := make(map[uint32]chan inputWithLock)
 
 	for {
 		select {
-		case in := <-daemonCh:
-			switch in.orderType {
+		case data := <-daemonCh:
+			switch data.in.orderType {
 			case inputCancel:
-				ch, ok := idToInstrumentCh[in.orderId]
+				ch, ok := idToInstrumentCh[data.in.orderId]
 				if !ok {
-					outputOrderDeleted(in, false, GetCurrentTimestamp())
+					outputOrderDeleted(data.in, false, GetCurrentTimestamp())
 					continue
 				}
 
-				ch <- in
+				ch <- data
 			default:
-				ch, ok := nameToInstrumentCh[in.instrument]
+				ch, ok := nameToInstrumentCh[data.in.instrument]
 				if !ok {
-					ch = make(chan input, 100000)
-					nameToInstrumentCh[in.instrument] = ch
+					ch = make(chan inputWithLock, 100000)
+					nameToInstrumentCh[data.in.instrument] = ch
 					go instrument(ch)
 				}
-				idToInstrumentCh[in.orderId] = ch
+				idToInstrumentCh[data.in.orderId] = ch
 
-				ch <- in
+				ch <- data
 			}
 		}
 	}
 }
 
-func instrument(ch chan input) {
+func instrument(ch chan inputWithLock) {
 	orderBuy := &OrderBookBuy{}
 	heap.Init(orderBuy)
 	orderSell := &OrderBookSell{}
 	heap.Init(orderSell)
 
 	for {
-		in := <-ch
-		switch in.orderType {
+		data := <-ch
+		switch data.in.orderType {
 		case inputBuy:
-			for in.count > 0 {
+			for data.in.count > 0 {
 				if len(*orderSell) == 0 {
 					break
 				}
 
-				if (*orderSell)[0].price > in.price {
+				if (*orderSell)[0].price > data.in.price {
 					break
 				}
 
-				if (*orderSell)[0].count > in.count {
-					outputOrderExecuted((*orderSell)[0].orderId, in.orderId, (*orderSell)[0].execId, (*orderSell)[0].price, in.count, GetCurrentTimestamp())
+				if (*orderSell)[0].count > data.in.count {
+					outputOrderExecuted((*orderSell)[0].orderId, data.in.orderId, (*orderSell)[0].execId, (*orderSell)[0].price, data.in.count, GetCurrentTimestamp())
 					(*orderSell)[0].execId++
-					(*orderSell)[0].count -= in.count
-					in.count = 0
+					(*orderSell)[0].count -= data.in.count
+					data.in.count = 0
 				} else {
-					outputOrderExecuted((*orderSell)[0].orderId, in.orderId, (*orderSell)[0].execId, (*orderSell)[0].price, (*orderSell)[0].count, GetCurrentTimestamp())
-					in.count -= (*orderSell)[0].count
+					outputOrderExecuted((*orderSell)[0].orderId, data.in.orderId, (*orderSell)[0].execId, (*orderSell)[0].price, (*orderSell)[0].count, GetCurrentTimestamp())
+					data.in.count -= (*orderSell)[0].count
 					heap.Pop(orderSell)
 				}
 			}
-			if in.count > 0 {
-				heap.Push(orderBuy, orderBookType{in.orderId, 1, in.price, in.count, GetCurrentTimestamp()})
-				outputOrderAdded(in, GetCurrentTimestamp())
+			if data.in.count > 0 {
+				heap.Push(orderBuy, orderBookType{data.in.orderId, 1, data.in.price, data.in.count, GetCurrentTimestamp()})
+				outputOrderAdded(data.in, GetCurrentTimestamp())
 			}
 		case inputSell:
-			for in.count > 0 {
+			for data.in.count > 0 {
 				if len(*orderBuy) == 0 {
 					break
 				}
 
-				if (*orderBuy)[0].price < in.price {
+				if (*orderBuy)[0].price < data.in.price {
 					break
 				}
 
-				if (*orderBuy)[0].count > in.count {
-					outputOrderExecuted((*orderBuy)[0].orderId, in.orderId, (*orderBuy)[0].execId, (*orderBuy)[0].price, in.count, GetCurrentTimestamp())
+				if (*orderBuy)[0].count > data.in.count {
+					outputOrderExecuted((*orderBuy)[0].orderId, data.in.orderId, (*orderBuy)[0].execId, (*orderBuy)[0].price, data.in.count, GetCurrentTimestamp())
 					(*orderBuy)[0].execId++
-					(*orderBuy)[0].count -= in.count
-					in.count = 0
+					(*orderBuy)[0].count -= data.in.count
+					data.in.count = 0
 				} else {
-					outputOrderExecuted((*orderBuy)[0].orderId, in.orderId, (*orderBuy)[0].execId, (*orderBuy)[0].price, (*orderBuy)[0].count, GetCurrentTimestamp())
-					in.count -= (*orderBuy)[0].count
+					outputOrderExecuted((*orderBuy)[0].orderId, data.in.orderId, (*orderBuy)[0].execId, (*orderBuy)[0].price, (*orderBuy)[0].count, GetCurrentTimestamp())
+					data.in.count -= (*orderBuy)[0].count
 					heap.Pop(orderBuy)
 				}
 			}
-			if in.count > 0 {
-				heap.Push(orderSell, orderBookType{in.orderId, 1, in.price, in.count, GetCurrentTimestamp()})
-				outputOrderAdded(in, GetCurrentTimestamp())
+			if data.in.count > 0 {
+				heap.Push(orderSell, orderBookType{data.in.orderId, 1, data.in.price, data.in.count, GetCurrentTimestamp()})
+				outputOrderAdded(data.in, GetCurrentTimestamp())
 			}
 		case inputCancel:
 			flag := false
 
 			for i := 0; i < len(*orderBuy); i++ {
-				if (*orderBuy)[i].orderId == in.orderId {
+				if (*orderBuy)[i].orderId == data.in.orderId {
 					heap.Remove(orderBuy, i)
-					outputOrderDeleted(in, true, GetCurrentTimestamp())
+					outputOrderDeleted(data.in, true, GetCurrentTimestamp())
 					flag = true
 					break
 				}
 			}
 			for i := 0; i < len(*orderSell); i++ {
-				if (*orderSell)[i].orderId == in.orderId {
+				if (*orderSell)[i].orderId == data.in.orderId {
 					heap.Remove(orderSell, i)
-					outputOrderDeleted(in, true, GetCurrentTimestamp())
+					outputOrderDeleted(data.in, true, GetCurrentTimestamp())
 					flag = true
 					break
 				}
 			}
 
 			if !flag {
-				outputOrderDeleted(in, false, GetCurrentTimestamp())
+				outputOrderDeleted(data.in, false, GetCurrentTimestamp())
 			}
 		}
+		<-data.mtx
 	}
 }
 
@@ -151,6 +152,7 @@ func (e *Engine) accept(ctx context.Context, conn net.Conn) {
 
 func handleConn(conn net.Conn) {
 	defer conn.Close()
+	mtx := make(chan struct{}, 1)
 	for {
 		in, err := readInput(conn)
 		if err != nil {
@@ -159,14 +161,15 @@ func handleConn(conn net.Conn) {
 			}
 			return
 		}
+		mtx <- struct{}{}
 		switch in.orderType {
 		case inputCancel:
 			fmt.Fprintf(os.Stderr, "Got cancel ID: %v\n", in.orderId)
-			daemonCh <- in
+			daemonCh <- inputWithLock{in, mtx}
 		default:
 			fmt.Fprintf(os.Stderr, "Got order: %c %v x %v @ %v ID: %v\n",
 				in.orderType, in.instrument, in.count, in.price, in.orderId)
-			daemonCh <- in
+			daemonCh <- inputWithLock{in, mtx}
 		}
 	}
 }
